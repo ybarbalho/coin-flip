890beb1f337a939fe476ba5acdb39da639ad390c
ybarbalho <yuri.barbalho48@gmail.com>
2025-05-21 15:44:51 -0300

    new commit

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Instale bibliotecas do sistema necessárias antes do pip install
+apt-get update && apt-get install -y \
+    build-essential \
+    python3-dev \
+    libopenblas-dev \
+    liblapack-dev \
+    gfortran \
+    pkg-config \
+    git \
+    && rm -rf /var/lib/apt/lists/*
+
+# Continue com a instalação de dependências Python
+pip install -r requirements.txt
