ea983140d57646fe5e05276a88b33222b886d38a
ybarbalho <yuri.barbalho48@gmail.com>
2025-05-21 16:56:02 -0300

    version 1

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import pandas as pd
 import scipy.stats
 import streamlit as st
 import time
+
+# estas são variáveis persistentes preservadas à medida que o Streamlin executa novamente esse script
+if 'experiment_no' not in st.session_state:
+    st.session_state['experiment_no'] = 0
+
+if 'df_experiment_results' not in st.session_state:
+    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iterations', 'mean'])
 
 st.header('Jogando uma moeda')
 
@@ -15,7 +23,7 @@ def toss_coin(n):
     outcome_1_count = 0
 
     for r in trial_outcomes:
-        outcome_no += 1
+        outcome_no +=1
         if r == 1:
             outcome_1_count += 1
         mean = outcome_1_count / outcome_no
@@ -24,8 +32,22 @@ def toss_coin(n):
 
     return mean
 
-number_of_trials = st.slider('Número de tentativas:', 1, 1000, 10)
+number_of_trials = st.slider('Número de tentativas?', 1, 1000, 10)
 start_button = st.button('Executar')
 
 if start_button:
     st.write(f'Executando o experimento de {number_of_trials} tentativas.')
+    st.session_state['experiment_no'] += 1
+    mean = toss_coin(number_of_trials)
+    st.session_state['df_experiment_results'] = pd.concat([
+        st.session_state['df_experiment_results'],
+        pd.DataFrame(data=[[st.session_state['experiment_no'],
+                            number_of_trials,
+                            mean]],
+                     columns=['no', 'iterations', 'mean'])
+        ],
+        axis=0)
+    st.session_state['df_experiment_results'] = \
+        st.session_state['df_experiment_results'].reset_index(drop=True)
+
+st.write(st.session_state['df_experiment_results'])
