adc33f3323802f0c7e9cf0f5861e9c0bb868d989
ybarbalho <yuri.barbalho48@gmail.com>
2025-05-21 16:47:52 -0300

    new version of app.py

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
+import scipy.stats
 import streamlit as st
+import time
 
 st.header('Jogando uma moeda')
+
+chart = st.line_chart([0.5])
+
+def toss_coin(n):
+
+    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
+
+    mean = None
+    outcome_no = 0
+    outcome_1_count = 0
+
+    for r in trial_outcomes:
+        outcome_no += 1
+        if r == 1:
+            outcome_1_count += 1
+        mean = outcome_1_count / outcome_no
+        chart.add_rows([mean])
+        time.sleep(0.05)
+
+    return mean
 
 number_of_trials = st.slider('Número de tentativas:', 1, 1000, 10)
 start_button = st.button('Executar')
 
 if start_button:
     st.write(f'Executando o experimento de {number_of_trials} tentativas.')
-
-st.write('Ainda não é um aplicativo funcional. Em construção.')
